650bb93662f016794b3f646a146de2f36531a48f
Nicole Groeger <nicole.groeger@triosstudent.com>
2024-08-22 08:19:45 -0400

    added code to MapsActiviy.kt

--- app/src/main/java/com/triosng2024/treasurehunt/MapsActivity.kt
+++ app/src/main/java/com/triosng2024/treasurehunt/MapsActivity.kt
@@ -2,18 +2,29 @@ package com.triosng2024.treasurehunt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import android.Manifest
+import android.content.pm.PackageManager
+import android.util.Log
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.LatLng
 import com.triosng2024.treasurehunt.databinding.ActivityMapsBinding
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var binding: ActivityMapsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,6 +37,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        setupLocationClient()
     }
 
     /**
@@ -40,9 +52,75 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        // Add a marker to a local business and move the camera
+        val teaHouse = LatLng(43.725808,-79.722134) // not sure how many points are needed so I used what the getlatlong site had.
+        mMap.addMarker(MarkerOptions().position(teaHouse).title("Marker for Sushi Wong"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(teaHouse))
+
+        getCurrentLocation()
+    }
+
+    private fun setupLocationClient() {
+        fusedLocationClient =
+            LocationServices.getFusedLocationProviderClient(this)
+    }
+
+    private fun requestLocationPermissions() {
+        ActivityCompat.requestPermissions(this,
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION),
+            REQUEST_LOCATION)
+    }
+
+    companion object {
+        private const val REQUEST_LOCATION = 1
+        private const val TAG = "MapsActivity"
+    }
+
+    private fun getCurrentLocation() {
+        if ((ActivityCompat.checkSelfPermission(this,
+            Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED) ||
+            (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) !=
+                    PackageManager.PERMISSION_GRANTED))
+        {
+            requestLocationPermissions()
+        }
+        else
+        {
+         mMap.isMyLocationEnabled = true
+
+         fusedLocationClient.lastLocation.addOnCompleteListener {
+             val location = it.result
+             if(location != null) {
+                 val latLng = LatLng(location.latitude, location.longitude)
+                 val update = CameraUpdateFactory.newLatLngZoom(latLng, 14.0f)
+                 mMap.moveCamera(update)
+             }
+             else
+             {
+                 Log.e(TAG, "No location found")
+             }
+         }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions:Array<out String>,
+        grantResults:IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_LOCATION){
+            if(grantResults.size == 2 && grantResults[0] ==
+                PackageManager.PERMISSION_GRANTED && grantResults[1] ==
+                PackageManager.PERMISSION_GRANTED) {
+                getCurrentLocation()
+            }
+            else {
+                Log.e(TAG, "Location permission denied")
+            }
+        }
     }
 }
